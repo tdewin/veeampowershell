d1210e43b24ecb9a2a95e661469939ddef6dc700
tdewin <tdewin@users.noreply.github.com>
2015-08-26 18:07:08 +0200

    more control
    need retest though

--- doprep/bootstrap.ps1
+++ doprep/bootstrap.ps1
@@ -1,6 +1,9 @@
-﻿Get-Volume | ? { (($_.DriveType -eq "CD-ROM") -and ($_.FileSystem -eq "UDF")) } | % {
+﻿$myself = (Get-Variable MyInvocation).value.InvocationName
+Get-Volume | ? { (($_.DriveType -eq "CD-ROM") -and ($_.FileSystem -eq "UDF")) } | % {
     $doprep = ( "{0}:\doprep.ps1" -f $_.driveLetter)
-    & $doprep
+    if(Test-Path $doprep) {
+        mv $myself ("{0}.bak" -f $myself) 
+        & $doprep
+    }
 }
-$myself = (Get-Variable MyInvocation).value.InvocationName
-mv $myself ("{0}.bak" -f $myself) 
+
