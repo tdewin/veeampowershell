e2bc177d9c4e8d4781c9ecb006937f711f02e26d
tdewin <tdewin@users.noreply.github.com>
2015-11-27 11:40:26 +0100

    per app detect report

---
+++ veeam-per-app-detect.ps1
@@ -0,0 +1,49 @@
+$outfile = "c:\veeam-app-detection.html"
+
+asnp veeampssnapin
+$rps = Get-VBRRestorePoint
+
+$ids = $rps | % { $_.ObjectId } | Sort-Object | Get-Unique
+$apps = "HasAd","HasExchange","HasOracle","HasSQL","HasSharepoint"
+$applist = @{}
+$apps | % {
+    $applist[$_] = @()
+}
+
+$ids | % {
+    $id = $_
+    $rpwithid = $rps | ? { $_.ObjectId -eq $id } | Sort-Object -Property creationtime -Descending
+       
+    $apps | % {
+        $hasapp = $false
+        $app = $_
+        $detectedobject = 0
+        for($i=0; ($i -lt $rpwithid.Count) -and (-not $hasapp); $i++) {
+            $rpv = $rpwithid[$i]
+            if($rpv."$app")
+            {
+                $hasapp = $true
+                $detectedobject = New-Object -TypeName psobject -Property @{
+                    Application=($app -replace "^Has","");
+                    "VM Name"=$rpv.VmName;
+                    FQDN=$rpv.Fqdn;
+                    "Detection Date"=(get-date -format "yyyy/MM/dd" $rpv.CreationTime);
+                    "Location"=$rpv.AuxData.Location
+                }
+            }
+        }
+        if($hasapp) {
+            $applist[$app] += $detectedobject
+        }
+    }
+}
+
+$header = "<title>Veeam App Detection</title><style>body { font-family:arial } h1 { color:white;background-color:green; } table {border-collapse: collapse; } td,table,th { border: 1px solid black;padding:2px; } th { text-align:left; } .thVMName { width:100px; } .thFQDN { width:200px; } </style>"
+$html = ""
+$applist.Keys | Sort-Object | % {
+    $html += ("<h1>{0}</h1>" -f ($_ -replace "^Has",""))
+    $html += ($applist[$_] | ConvertTo-Html -Property "Detection Date","Location","VM Name","FQDN" -Fragment) -replace "<th>([^<]*)</th>",'<th class=''th$1''>$1</th>'
+    $html += "<br><br>"
+}  
+ConvertTo-Html -head $header -Body $html > $outfile
+ 
