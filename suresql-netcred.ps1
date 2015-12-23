aebc8c7c3fb49b1fef8ce744ebe258d80d5e6ff5
tdewin <tdewin@users.noreply.github.com>
2015-12-23 11:21:49 +0100

    suresql
    some bug fixes and minimumdb return statement

--- suresql-netcred.ps1
+++ suresql-netcred.ps1
@@ -1,9 +1,10 @@
 param(
-	$server = "localhost"
-	$instance = "$server\MSSQLSERVER"
+	$server = "localhost",
+	$instance = "$server\MSSQLSERVER",
+    $minimumdb = 4
 )
 
-$connectionString = "Server=$instance;Integrated Security=True;"
+$connectionString = “Server=$instance;Integrated Security=True;”
 
 $connection = New-Object System.Data.SqlClient.SqlConnection
 $connection.ConnectionString = $connectionString
@@ -27,18 +28,25 @@ try {
     $datatable.Load($reader)
 
     $connection.Close()
-    $allonline = $true
-    $datatable | % {
-        if ( $_[2] -eq 0 ) {
-            write-host ("online : {0}" -f $_[0])
-        } else {
-            $allonline = $false
-            #state codes : https://msdn.microsoft.com/en-us/library/ms178534.aspx
-            write-host ("code {0} : {1}" -f $_[2],$_[0])
+    
+    if($datatable.Rows.Count -ge $minimumdb) {
+        $allonline = $true
+        $off = @()
+        $datatable.Rows | % {
+            if ( $_[2] -eq 0 ) {
+                write-host ("online : {0}" -f $_[0])
+            } else {
+                $allonline = $false
+                #state codes : https://msdn.microsoft.com/en-us/library/ms178534.aspx
+                write-host ("code {0} : {1}" -f $_[2],$_[0])
+                $off += $_[0]
+            }
         }
+        if ($allonline) { $failure = 0; write-host ("All {0} databases online !" -f $datatable.count) } 
+        else { $failure = 1;write-host ("Not online db detected : {0}" -f ($off -join ",")) }
+    } else {
+        $failure = 1;write-host ("Query should at least give back {0} databases" -f $minimumdb)
     }
-    if ($allonline) { $failure = 0; write-host "All online!" } 
-    else { $failure = 1;write-host "Not online db detected" }
 } catch {
     write-host ("Query failed or could not open connection {0} " -f $error[0])
     $failure = 1
