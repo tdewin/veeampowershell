a196a6bed48f7431eee0a3f7384980c0217ffa17
tdewin <tdewin@users.noreply.github.com>
2015-12-24 11:49:02 +0100

    suresql netcred
    more stability incase of failure and still giving full overview in
    surebackup logs (if one db fails, the other ones should still be tested)

--- suresql-netcred.ps1
+++ suresql-netcred.ps1
@@ -12,33 +12,62 @@ $connection = New-Object System.Data.SqlClient.SqlConnection
 $connection.ConnectionString = $connectionString
 
 $failure = 1
+$mustfail = $false
 
-#ugly fix to touch all dbs, otherwise it shows corrupted db's as online :(
-$useall = 'EXECUTE master.sys.sp_MSforeachdb ''USE [?];'''
 $q = 'use master;SELECT name, state FROM sys.databases;'
 
 
 try {
     $connection.Open()
 
-    
-    $cmd = $connection.CreateCommand()
-    $cmd.CommandText = $useall
-    $cmd.ExecuteNonQuery()
-
+    write-host ("Connected on {0}" -f $connectionString)
+    #As long as there was no connection to the database, it is set 'online' until the point it has been conneted
+    #so first list the databases
+    #try to query there tables, and then reread the databases status
+    $datatable = @()
     $cmd = $connection.CreateCommand()
     $cmd.CommandText = $q
-
     $reader = $cmd.ExecuteReader()
-    $datatable = @()
-    while($reader.Read())
-    {
+    while($reader.Read()) {
         $datatable += New-Object -TypeName psobject -Property @{name=$reader["name"];state=$reader["state"];}
     }
+    $reader.Close()
 
-    $connection.Close()
-    
     if($datatable.Count -ge $minimumdb) {
+        $datatable | % {
+                $dbname = $_.name
+                $lcmd = $connection.CreateCommand()
+                $lcmd.CommandText = ("USE [{0}];select name from sys.tables;" -f $dbname )
+                try {
+                    $null = $lcmd.ExecuteScalar()
+               
+                } catch { 
+                    $mustfail = $true
+                    write-host ("Something bad on {0}, ignoring for now {1}" -f $dbname,$error[0]) 
+                    
+                    if($connection.state -ne 'Open')
+                    {
+                        write-host "Need to reopen the db, ..."
+                        try { $connection.Open() } catch { write-host "Something is really bad, exiting with failure";exit 1 }
+                    }
+                }
+        }
+    
+
+        #rereading the table
+        $datatable = @()
+        $cmd = $connection.CreateCommand()
+        $cmd.CommandText = $q
+
+        $reader = $cmd.ExecuteReader()
+        while($reader.Read())
+        {
+            $datatable += New-Object -TypeName psobject -Property @{name=$reader["name"];state=$reader["state"];}
+        }
+        $reader.Close()
+        $connection.Close()
+    
+    
         $allonline = $true
         $off = @()
         $datatable | % {
@@ -51,7 +80,8 @@ try {
                 $off += $_.name
             }
         }
-        if ($allonline) { $failure = 0; write-host ("All {0} databases online !" -f $datatable.count) } 
+        if ($allonline -and (-not $mustfail) ) { $failure = 0; write-host ("All {0} databases online !" -f $datatable.count) } 
+        elseif ($allonline -and $mustfail) { $failure = 1;write-host ("All {0} databases online where online but initial query on all dbs failed somewhere, please look for ""Something bad on""" -f $datatable.count)}
         else { $failure = 1;write-host ("Not online db detected : {0}" -f ($off -join ",")) }
     } else {
         $failure = 1;write-host ("Query should at least give back {0} databases" -f $minimumdb)
